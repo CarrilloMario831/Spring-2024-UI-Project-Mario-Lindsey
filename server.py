041181057790ed1a428b6b010fab26be775aa255
CarrilloMario831 <carrillomario831@gmail.com>
2024-04-22 15:31:49 -0400

    Initial Push

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify, Markup
+app = Flask(__name__)
+
+# ROUTES
+@app.route('/')
+def homepage():
+   return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
